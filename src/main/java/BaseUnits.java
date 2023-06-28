8cd357cbe9103f3010bb6f4155a8b9b29f8270ae
sergey <sergey@solaev.ru>
2023-06-28 21:56:32 +0400

    Initial commit

---
+++ src/main/java/BaseUnits.java
@@ -0,0 +1,18 @@
+public abstract class BaseUnits {
+
+    String name;
+    int level;
+    int initiative;
+    int armor;
+    int hit;
+
+    public int meleeStrike(){
+        return ;
+    }
+
+     
+
+
+
+
+}
